dba33d17dbe4b28835896f28517fdc2e3411f543
BrenoSS <BrenoSS@users.noreply.github.com>
2015-05-26 22:46:36 -0300

    Update view_inf_empresa.sql

--- view_inf_empresa.sql
+++ view_inf_empresa.sql
@@ -1,6 +1,11 @@
 -- View: inf_empresa
--- ~pequeno resumo~
--- Autor: 
+-- Resumo: view que seleciona os atributos razao_social, email, CNPJ, rua, numero, bairro, municipio, estado, Cep das empresas cadastradas no BD
+-- Autor: Breno
+
+IF EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS
+        WHERE TABLE_NAME = 'inf_empresa')
+    DROP VIEW inf_empresa
+GO
 
 create view inf_empresa as
 		select razao_social, email, CNPJ, rua, numero, bairro, municipio, estado, cep
