e54985eb120cd447211097d76141ad2c0a1f3535
BrenoSS <BrenoSS@users.noreply.github.com>
2015-05-26 22:42:27 -0300

    Update view_inf_cliente.sql

--- view_inf_cliente.sql
+++ view_inf_cliente.sql
@@ -1,9 +1,13 @@
+--Nome: inf_cliente
+--Resumo: view que exibe informações CPF, nome completo, sexo e código cabelo do cliente
+--Autor: Breno
+
 IF EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS
         WHERE TABLE_NAME = 'inf_cliente')
     DROP VIEW inf_cliente
 GO
 
 create view inf_cliente as
-		select primeiro_nome, sobrenome, sexo, cod_cabelo, fisica.CPF
+		select fisica.CPF as CPF, primeiro_nome as Primeiro_Nome, sobrenome as Sobrenome, sexo, cod_cabelo 
 		from fisica, cliente
 		where fisica.CPF = cliente.CPF
