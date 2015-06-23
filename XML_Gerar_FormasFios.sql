82268873c431a2fc29dd9cdf4c5c31bee1497f8c
DouglasBarbino <DouglasBarbino@users.noreply.github.com>
2015-06-23 09:31:50 -0300

    Update XML_Gerar_FormasFios.sql

--- XML_Gerar_FormasFios.sql
+++ XML_Gerar_FormasFios.sql
@@ -189,7 +189,7 @@ CREATE XML SCHEMA COLLECTION XMLSchemaProdutoVenda AS '
 			<xsd:element name="precoFabrica" minOccurs="0" maxOccurs="1">
 				<xsd:simpleType>
 					<xsd:restriction base="xsd:decimal">
-						<xsd:totalDigits value="20"/>
+						<xsd:totalDigits value="4"/>
 						<xsd:fractionDigits value="2"/>
 					</xsd:restriction>
 				</xsd:simpleType>
